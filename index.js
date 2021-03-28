84ff564b0730addd6e2ce819076736a92c5dae8d
ahnafabir <ahnafabir313@gmail.com>
2021-03-28 06:06:05 +0600

    port updated and nodemod fixed

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
   res.send('Hello, Welcome to Ema-Jhon server');
